c608c7279861237a84c1b16649a50cd628e0ef75
mdhasan124677-lang <mdhasan124677@gmail.com>
2025-08-27 18:25:20 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
